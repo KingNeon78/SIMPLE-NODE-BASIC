b76e6d4834797c7c1f2e82a4ac1772d76cf41c64
KingNeon78 <111181442+KingNeon78@users.noreply.github.com>
2023-02-25 13:06:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log(" welcom");
+
+function add(x,y){
+    let z=x+y;
+    console.log(z);
+    
+}
+add(10,5); 
+
+function fnc(name, age){
+    console.log(`my name is ${name} and my age s ${age} `);
+
+}
+fnc("GUlam", 21);
+
+const calname= (name) =>{
+    console.log(name);
+} 
+calname(" i Love MY SELF")
